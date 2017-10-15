966bc9845ae3b381a7aab4d1a5cb57c75f578f18
imdurgadas <durgadasthegenius@gmail.com>
2017-10-15 14:06:04 +0530

    Spring boot starter

---
+++ src/main/java/com/durgadas/springboot/topic/TopicService.java
@@ -0,0 +1,22 @@
+package com.durgadas.springboot.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = Arrays.asList(new Topic("1", "Java", "Java OO Concepts"),
+			new Topic("2", "Angular", "Getting Started with Angular"));
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+}
