34cca41cc7a32ec29ac5a7c944232cdd05f50556
imdurgadas <durgadasthegenius@gmail.com>
2017-10-15 15:01:54 +0530

    Add POST implementation

--- src/main/java/com/durgadas/springboot/topic/TopicService.java
+++ src/main/java/com/durgadas/springboot/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.durgadas.springboot.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,8 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("1", "Java", "Java OO Concepts"),
-			new Topic("2", "Angular", "Getting Started with Angular"));
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(new Topic("1", "Java", "Java OO Concepts"),
+			new Topic("2", "Angular", "Getting Started with Angular")));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -17,6 +18,10 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
