8c4895b0042e49f3a9ecc62f1b359f83853bf151
imdurgadas <durgadasthegenius@gmail.com>
2017-10-15 17:14:53 +0530

    Add DAO implementation for findAll and add

--- src/main/java/com/durgadas/springboot/topic/TopicService.java
+++ src/main/java/com/durgadas/springboot/topic/TopicService.java
@@ -5,15 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
+	
+	@Autowired
+	TopicRepository topicRepository;
 
 	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(new Topic("1", "Java", "Java OO Concepts"),
 			new Topic("2", "Angular", "Getting Started with Angular")));
 	
 	public List<Topic> getAllTopics(){
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 	
@@ -22,7 +28,7 @@ public class TopicService {
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 	
 	public void updateTopic(String id , Topic topic) {
