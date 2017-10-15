53e32e17dbe84420cd76ee23c0ed830b4d737d53
imdurgadas <durgadasthegenius@gmail.com>
2017-10-15 19:27:23 +0530

    Refactor Controller to use Method Mapping annotation and custom httpstatus

--- src/main/java/com/durgadas/springboot/topic/TopicController.java
+++ src/main/java/com/durgadas/springboot/topic/TopicController.java
@@ -3,45 +3,55 @@ package com.durgadas.springboot.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/topics")
 public class TopicController {
 	
 	@Autowired
 	TopicService topicService;
 
-	@RequestMapping("/topics")
+	@GetMapping
 	public List<Topic> getAllTopics(@RequestParam (name="name",required=false) String name){
 		if (null != name) {
 			return topicService.getTopicsByName(name);
 		}
 		return topicService.getAllTopics();
+		
 	}
 	
-	@RequestMapping("/topics/{id}")
+	@GetMapping("/{id}")
 	public Topic getTopicById(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
 	
-	@RequestMapping(value="/topics", method=RequestMethod.POST)
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
 	
-	@RequestMapping(value="/topics/{id}", method=RequestMethod.PUT)
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
 		topicService.updateTopic(id, topic);
 	}
 	
 
-	@RequestMapping(value="/topics/{id}", method=RequestMethod.DELETE)
-	public void updateTopic(@PathVariable String id) {
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteTopic(@PathVariable String id) {
 		topicService.deleteTopic(id);
 	}
 }
